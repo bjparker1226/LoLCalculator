cad1d8a1e6526976066432a2c8aca41489dee0e4
Benjamin <benparker1226@gmail.com>
2023-09-21 09:22:44 -0400

    Revert "feat: database setup"
    This reverts commit 7a4e32f15de434013e69f09d842fbb2c9e981ce1.

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
 
-class ToDo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 @app.route('/')
 def index():
     return render_template('index.html')
