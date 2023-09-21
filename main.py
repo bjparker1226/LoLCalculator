ad9ee2c585093d240dc78d2c865f3c7ebcb4bbdf
Benjamin <benparker1226@gmail.com>
2023-09-21 09:43:54 -0400

    feat: undo previous test

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ class ToDo(db.Model):
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        print('pp')
         return '<Task %r>' % self.id
 
 @app.route('/')
